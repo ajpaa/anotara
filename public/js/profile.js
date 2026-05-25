04d63857a68bdea88977c4c138f3a49aff26f6f9
unknown <jcespino3@up.edu.ph>
2026-05-26 02:23:23 +0800

    with profile fr

---
+++ public/js/profile.js
@@ -0,0 +1,60 @@
+const API = "/guest";
+
+// calendar - wala pang click feature 'to
+const monthYear = document.querySelector(".month-year");
+const dates = document.querySelector(".dates");
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentDate = new Date();
+
+const updateCalendar = () => {
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+
+    const firstDay = new Date(currentYear, currentMonth, 1);
+    const lastDay = new Date(currentYear, currentMonth + 1, 0);
+    const totalDays = lastDay.getDate();
+
+    let firstDayIndex = firstDay.getDay() - 1; 
+    if (firstDayIndex < 0) firstDayIndex = 6; 
+
+
+    const monthYearString = currentDate.toLocaleString('default', { month: 'long', year: 'numeric' });
+    monthYear.textContent = monthYearString;
+
+    let dateHTML = '';
+    
+    
+    for (let i = 0; i < firstDayIndex; i++) {
+        dateHTML += `<div class="date inactive"></div>`;
+    }
+
+    for (let i = 1; i <= totalDays; i++) {
+        const date = new Date(currentYear, currentMonth, i);
+        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+        dateHTML += `<div class="date ${activeClass}">${i}</div>`;
+    }
+    
+    dates.innerHTML = dateHTML;
+}
+
+prevBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    updateCalendar();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    updateCalendar();
+});
+
+updateCalendar();
+
+async function loadGuests() {
+    const res = await fetch(API);
+    let data = await res.json();
+
+    const guestName = document.getElementById('guest-name');
+    guestName.innerHTML = "";
+}
