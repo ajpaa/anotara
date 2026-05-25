9f8ce4f0d3cabe0c51edeb3c0d9c92493a1d1c53
Princess Ang <101443372+bibble-exe@users.noreply.github.com>
2026-05-26 00:52:21 +0800

    fixed login and signup page

--- models/host.js
+++ models/host.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const hostSchema = new mongoose.Schema({
     hostName: { type: String, required: true },
     hostContactNumber: { type: String, required: true },
-    adminID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    adminID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Host', hostSchema);
