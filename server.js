7e2380ed36d1418d31663b0427652d42b274bcd3
Alyssa <alyssaerinpaa@gmail.com>
2026-05-27 03:40:55 +0800

    test

--- server.js
+++ server.js
@@ -170,13 +170,13 @@ app.get(adminPages, isAuthenticated, hasRole('admin'), (req, res) => {
     
     let targetFile = path.basename(cleanPath); 
     
-    // // If the request evaluates to empty or just "admin", default to "admin.html"
-    // if (!targetFile || targetFile === 'admin') {
-    //     targetFile = 'admin.html';
-    // }
+    // If the request evaluates to empty or just "admin", default to "admin.html"
+    if (!targetFile || targetFile === 'admin') {
+        targetFile = 'admin.html';
+    }
     
     // Serves the files directly out of your public/admin/ folder structure
-    res.sendFile(path.join(__dirname, 'public', 'admin', targetFile));
+    res.sendFile(path.join(__dirname, 'public', 'admin'));
 });
 // ==========================================
 // 7. PUBLIC STATIC FILE HIGHWAY (MUST BE AFTER PROTECTED ROUTES)
